1b1fb67d0a55f20cff9e2f67e7ba10848e05a816
falsechicken <jmdevsupport@gmail.com>
2017-04-20 20:10:46 -0400

    - Added SetDebugMode.

--- glogger.go
+++ glogger.go
@@ -12,13 +12,15 @@ const (
 	Error
 )
 
-//Logs a message to the console.
+var debugMode = false
+
+//LogMessage Logs a message to the console.
 func LogMessage(level int, message string) {
 	if level == Debug {
-		fmt.Println( time.Now().String() + " | DEBUG: " + message)
+		fmt.Println(time.Now().String() + " | DEBUG: " + message)
 	}
 	if level == Info {
-		fmt.Println( time.Now().String() + " | INFO: " + message)
+		fmt.Println(time.Now().String() + " | INFO: " + message)
 	}
 	if level == Warning {
 		fmt.Println(time.Now().String() + " | WARNING: " + message)
@@ -26,4 +28,9 @@ func LogMessage(level int, message string) {
 	if level == Error {
 		fmt.Println(time.Now().String() + " | DEBUG: " + message)
 	}
+}
+
+//SetDebugMode enables or disables debug messages from being printed to the console.
+func SetDebugMode(dMode bool) {
+	debugMode = dMode
 }
