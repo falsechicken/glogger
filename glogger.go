706f3a71a99267f8cbe6768b82a4ffb6b2a29423
falsechicken <jmdevsupport@gmail.com>
2018-05-04 16:59:46 -0400

    - Fixed time formatting.

--- glogger.go
+++ glogger.go
@@ -18,16 +18,16 @@ var debugMode = false
 //LogMessage Logs a message to the console.
 func LogMessage(level int, message string) {
 	if level == Debug && debugMode {
-		fmt.Println(time.Now().String() + " | DEBUG: " + message)
+		fmt.Println(time.Now().Format(time.RFC822) + " | DEBUG: " + message)
 	}
 	if level == Info {
-		fmt.Println(time.Now().String() + " | INFO: " + message)
+		fmt.Println(time.Now().Format(time.RFC822) + " | INFO: " + message)
 	}
 	if level == Warning {
-		fmt.Println(time.Now().String() + " | WARNING: " + message)
+		fmt.Println(time.Now().Format(time.RFC822) + " | WARNING: " + message)
 	}
 	if level == Error {
-		fmt.Println(time.Now().String() + " | DEBUG: " + message)
+		fmt.Println(time.Now().Format(time.RFC822) + " | DEBUG: " + message)
 	}
 }
 
